63193fd7010965c98bd35d8649b5668a63f55d89
Maxim Weinrauch <bugimen25@gmail.com>
2021-08-20 17:16:20 +0200

    Implemented logic to get AVG pixel color

--- WallpaperManager.py
+++ WallpaperManager.py
@@ -7,19 +7,26 @@ from PIL import Image
 def get_current_wallpaper():
     current_os = platform.system().lower()
     if current_os == "linux":
-        image = Image.open('/usr/share/backgrounds/brad-huchteman-stone-mountain.jpg')
-        image.show()
-
+        image = Image.open('/usr/share/backgrounds/ryan-stone-skykomish-river.jpg')
+        blur_image = image.filter(PIL.ImageFilter.GaussianBlur(100))
         width, height = image.size
 
-        rgb_img = image.convert('RGB')
-        for x in range(width):
-            for y in range(height):
-                r, g, b = rgb_img.getpixel((x, y))
-                print(r, g, b)
+        rgb_img = blur_image.convert('RGB')
 
-        blur_image = image.filter(PIL.ImageFilter.GaussianBlur(100))
-        blur_image.show()
+        steps = 100
+        r, g, b = 0, 0, 0
+        for x in range(0, width, steps):
+            for y in range(0, height, steps):
+                a, b, c = rgb_img.getpixel((x, y))
+                r = r + a
+                g = g + b
+                b = b + c
+
+        r = round(r / ((width * height) / steps) * 100)
+        g = round(g / ((width * height) / steps) * 100)
+        b = round(b / ((width * height) / steps) * 100)
+        print(r, g, b)
+
     elif current_os == "windows":
         print("hello world")
 
