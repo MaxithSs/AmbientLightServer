0c5e58526c5a35ad61815b2cc9eb36f0935ab933
Maxim Weinrauch <bugimen25@gmail.com>
2021-08-23 17:25:36 +0200

    renamed file for deploy

--- HttpRequestHandler.py
+++ HttpRequestHandler.py
@@ -4,7 +4,6 @@ import WallpaperManager
 
 
 class HttpRequestHandler(http.server.SimpleHTTPRequestHandler):
-    # todo - send every response in json format
     def do_GET(self) -> None:
         self.send_response(200)
         self.send_header("Content-type", "text/html")
